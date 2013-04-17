de4f9101a75a390e34b73e6f1a44fa9180e757d0
Rahul Gupta <rahulgupta2025@gmail.com>
2013-04-17 11:28:30 +0530

    Tool Upgraded

--- TooL/website/app/fetch.php
+++ TooL/website/app/fetch.php
@@ -1,34 +1,44 @@
 <?php
-	mysql_connect('localhost','root','root') or
+	mysql_connect('localhost','bopl','bopl') or
     die("Could not connect: " . mysql_error());;
 	mysql_select_db('bopl');
 	if(isset($_GET['type'])){
 		$type =$_GET['type'];
 	} else {
-		$type = "lang";
+		$type = "attribute";
 	}
 	if(isset($_GET['param'])){
 		$param = $_GET['param'];
 	} else {
-		$param = "c";
+		$param = "compile_memory";
 	}
+	$return="<table>";
 	switch($type){
 		case "lang":
 				$table = "lang";
 			break;
+		case "attribute":
+				$table= "Final_".$param;
+				$return .= "<tr><th>ID</th><th>C</th><th>C++</th><th>Haskell</th><th>Java</th><th>Python</th><th>Cython</th></tr>";
+				break;
 
 	}
 	switch ($param){
 		case "c":
 				$clause = "";
 				break;
+		default:
+				$clause="";
 	}
 	$query = "select * from $table ".$clause;
 	$result=mysql_query($query);
-	$return="";
-	while($row=mysql_fetch_array($result)){
+	while($row=mysql_fetch_row($result)){
 		$return .= "<tr>";
+		for($j = 0; $j < count($row); $j++) {
+     		   $return .= "<td>".$row[$j]."</td>";
+    		}	
 		$return .= "</tr>";
 	}
-
+	$return .= "</table>";
+	echo $return;
 ?>
--- TooL/website/app/scripts/main.js
+++ TooL/website/app/scripts/main.js
@@ -15,5 +15,47 @@ require(['app', 'jquery', 'bootstrap'], function (app, $) {
     'use strict';
     // use app here
     console.log(app);
+	var options=Array();
+	options['attribute']=[
+		{ val:"compile_memory" , text:"Compile Memory"},
+		{ val:"compile_time" , text:"Compile Time"},
+		{ val:"LOC" , text:"Lines of Code"},
+		{ val:"run_memory" , text:"Runtime Memory"},
+		{ val:"run_time" , text:"Run Time"}
+	];
+	options['domain']=[
+		{ val:"basic" , text:"Basic"},
+		{ val:"graph" , text:"Graph Algorithms"},
+		{ val:"misc" , text:"Miscellaneous"},
+		{ val:"np" , text:"NP - Hard"},
+		{ val:"numerical" , text:"Numerical Computation"},
+		{ val:"web", text:"Web Programming"}
+	];
+	options['language']=[
+		{ val:"c" , text:"C"},
+		{ val:"cpp" , text:"C++"},
+		{ val:"hs" , text:"Haskell"},
+		{ val:"java" , text:"JAVA"},
+		{ val:"python" , text:"Python"},
+		{ val:"cython" , text:"Cython"}
+	];
     console.log('Running jQuery %s', $().jquery);
+    $('#primary').change(function() {
+	var primary= $("select#primary option:selected").val();
+	$('#secondary').html("");
+	$.each(options[primary], function(index,opt){
+		$('#secondary').append('<option value="'+opt.val+'">'+opt.text+'</option>');
+	});
+	// Force change trigger for secondary
+	$('#secondary').change();
+    });
+    $('#secondary').change(function() {
+	var primary= $("select#primary option:selected").val();
+	var secondary= $("select#secondary option:selected").val();
+ 	$.ajax('fetch.php?type='+primary+'&param='+secondary).done(function(data){
+ 	$('#result').html(data);
+ 	});
+	});
+	// Force change trigger for primary
+    $('#primary').change();
 });
